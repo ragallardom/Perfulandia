7a3c4a9073c199a8c4e6bc575fcfc1eea37b7f9d
ragallardom <ragallardom@outlook.com>
2025-05-25 01:22:35 -0400

    Se agrega package Sale

---
+++ sale/src/test/java/cl/perfulandia/sale/SaleApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.perfulandia.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
