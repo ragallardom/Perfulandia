cf3e53d432d559622c9c8d36bb24383f078597c6
ragallardom <ragallardom@outlook.com>
2025-05-25 14:45:41 -0400

    Se agrega base de datos en servicio sale

--- sale/src/main/java/cl/perfulandia/sale/model/Sale.java
+++ sale/src/main/java/cl/perfulandia/sale/model/Sale.java
@@ -1,14 +1,13 @@
 package cl.perfulandia.sale.model;
 
 import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "sales")
 @Getter
 @Setter
 
@@ -17,14 +16,26 @@ public class Sale {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "userId", nullable = false)
     private Long userId;
+
+    @Column(name = "productId", nullable = false)
     private Long productId;
+
+    @Column(name = "branchId", nullable = false)
     private Long branchId;
+
+    @Column(nullable = false)
     private Integer quantity;
+
+    @Column(name = "unitPrice", nullable = false)
     private Double unitPrice;
+
+    @Column(name = "totalPrice", nullable = false)
     private Double totalPrice;
+
+    @Column(name = "timestamp", nullable = false)
     private LocalDateTime timestamp;
 
-    // getters and setters
 }
 
