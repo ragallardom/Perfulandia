40c4912c11571be996828cf65c768c03b8209666
Rodrigo Gallardo <167040497+ragallardom@users.noreply.github.com>
2025-07-04 11:23:37 -0400

    Mock JavaMailSender in BillingApplicationTests

--- billing/src/test/java/cl/perfulandia/billing/BillingApplicationTests.java
+++ billing/src/test/java/cl/perfulandia/billing/BillingApplicationTests.java
@@ -2,9 +2,13 @@ package cl.perfulandia.billing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class BillingApplicationTests {
+    @MockBean
+    private JavaMailSender mailSender;
     @Test
     void contextLoads() {
     }
